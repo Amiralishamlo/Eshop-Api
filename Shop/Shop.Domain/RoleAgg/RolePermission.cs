80e4c93911c5c42967544b99f4a6b6bb894ef44d
Amiralishamlo <amiralish00880@gmail.com>
2023-10-01 18:53:17 +0330

    init Role Aggregate & Seller Aggregate.

---
+++ Shop/Shop.Domain/RoleAgg/RolePermission.cs
@@ -0,0 +1,12 @@
+﻿using Common.Domain;
+using Shop.Domain.RoleAgg.Enums;
+
+namespace Shop.Domain.RoleAgg
+{
+    public class RolePermission:BaseEntity
+    {
+        public long RoleId { get; set; }
+        public Permission Permission { get; set; }
+
+    }
+}
