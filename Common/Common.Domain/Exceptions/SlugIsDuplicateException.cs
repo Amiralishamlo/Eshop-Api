4a46b3a749230f002b7a2dce318034da3df8e384
Amiralishamlo <amiralish00880@gmail.com>
2023-10-01 19:52:40 +0330

    init ProductAggregate.

---
+++ Common/Common.Domain/Exceptions/SlugIsDuplicateException.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Domain.Exceptions
+{
+    public class SlugIsDuplicateException : BaseDomainException
+    {
+        public SlugIsDuplicateException() : base("slug تکراری است")
+        {
+        }
+
+        public SlugIsDuplicateException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Shop/Shop.Domain/ProductAgg/Services/IProductDomainService.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop.Domain.ProductAgg.Services
+{
+    public interface IProductDomainService
+    {
+        bool SlugIsExist(string slug);
+    }
+}
