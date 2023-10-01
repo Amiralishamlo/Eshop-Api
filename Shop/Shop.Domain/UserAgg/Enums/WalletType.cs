5dff4db2c09a65cb0f3b3b5a35cb806fea3d1333
Amiralishamlo <amiralish00880@gmail.com>
2023-10-01 13:23:24 +0330

    init User Aggregate.

---
+++ Shop/Shop.Domain/UserAgg/Enums/WalletType.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Domain.UserAgg.Enums
+{
+    public enum WalletType
+    {
+        Deposit,
+        Withdrawal
+    }
+}
---
+++ Shop/Shop.Domain/UserAgg/Wallet.cs
@@ -0,0 +1,42 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Shop.Domain.UserAgg.Enums;
+
+namespace Shop.Domain.UserAgg
+{
+    public class Wallet : BaseEntity
+    {
+        public Wallet(int price, string desciption, bool isFinally, DateTime? finallyDate, WalletType type)
+        {
+            if (price < 500)
+                throw new InvalidDomainDataException();
+
+            Price = price;
+            Desciption = desciption;
+            IsFinally = isFinally;
+            FinallyDate = finallyDate;
+            Type = type;
+        }
+
+
+        public long UserId { get; internal set; }
+        public int Price { get; private set; }
+        public string Desciption { get; private set; }
+        public bool IsFinally { get; private set; }
+        public DateTime? FinallyDate { get; private set; }
+        public WalletType Type { get; private set; }
+
+        public void Finally(string refCode)
+        {
+            IsFinally = true;
+            FinallyDate = DateTime.Now;
+            Desciption += $" کد پیگیری : {refCode}";
+        }
+
+        public void Finally()
+        {
+            IsFinally = true;
+            FinallyDate = DateTime.Now;
+        }
+    }
+}
